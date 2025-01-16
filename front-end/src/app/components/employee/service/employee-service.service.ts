a93c1b3815748fd86d6157fb5da4a4e42a80b391
Huy Ho <huynino0496@gmail.com>
2025-01-17 00:11:46 +0700

    Added CreateEmployee function

--- front-end/src/app/components/employee/service/employee-service.service.ts
+++ front-end/src/app/components/employee/service/employee-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs';
+import * as fs from 'fs';
 
 @Injectable({
   providedIn: 'root',
@@ -21,8 +22,15 @@ export class EmployeeServiceService {
     )
   }
 
-  createEmployee(employee: any): Observable<any> {
-    this.employees.push(employee)
-    return of(employee)
+  createEmployee(employee: any): Observable<any> { 
+    const data = fs.readFileSync(this.apiUrl, 'utf-8'); 
+    const employees = JSON.parse(data); 
+   
+    employees.push(employee); 
+     
+    const updatedData = JSON.stringify(employees, null, 2); 
+    // Write the updated JSON back to the file 
+    fs.writeFileSync(this.apiUrl, updatedData); 
+    return of(employee); 
   }
 }
