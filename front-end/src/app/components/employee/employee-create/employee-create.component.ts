9fcd1664616931361966dbd49366e1eabcf524fe
nguyenhoangtuananh2002 <165981275+nguyenhoangtuananh2002@users.noreply.github.com>
2025-01-17 02:02:38 +0700

    Employee Create,Read

--- front-end/src/app/components/employee/employee-create/employee-create.component.ts
+++ front-end/src/app/components/employee/employee-create/employee-create.component.ts
@@ -1,35 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EmployeeServiceService } from '../service/employee-service.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
+interface Employee {
+  id: string;
+  avatar: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  birthday: string;
+  skills: string;
+  role: string;
+  is_admin: boolean;
+}
+
 @Component({
   selector: 'app-create-employee',
-  standalone:true,
-  imports:[FormsModule,CommonModule],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './employee-create.component.html',
   styleUrls: ['./employee-create.component.scss'],
 })
 export class EmployeeCreateComponent {
-  employee = {
-    name: '',
+  employees: Employee[] = [];
+  newEmployee = {
+    id: '',
+    avatar: '',
+    firstname: '',
+    lastname: '',
     email: '',
     birthday: '',
-    role: '',
     skills: '',
+    role: '',
     is_admin: false,
   };
 
   constructor(private employeeService: EmployeeServiceService) {}
 
-  onSubmit() {
-    const newEmployee = {
-      ...this.employee,
-      skills: this.employee.skills.split(',').map((skill) => skill.trim()), 
-    };
+  addEmployee(): void {
+    this.employeeService
+      .createEmployee(this.newEmployee)
+      .subscribe((employee) => {
+        this.employees.push(employee);
+        this.newEmployee = {
+          id: '',
+          avatar: '',
+          firstname: '',
+          lastname: '',
+          email: '',
+          birthday: '',
+          skills: '',
+          role: '',
+          is_admin: false,
+        };
+      });
+  }
 
-    this.employeeService.createEmployee(newEmployee).subscribe((response) => {
-      console.log('Employee created successfully:', response);
-    });
+  downloadJSON(): void {
+    const employees = this.employeeService.getEmployeesFromLocalStorage();
+    const json = JSON.stringify(employees, null, 2);
+    const blob = new Blob([json], { type: 'application/json' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'employees.json';
+    a.click();
+    window.URL.revokeObjectURL(url);
   }
 }
--- front-end/src/app/components/employee/service/employee-service.service.ts
+++ front-end/src/app/components/employee/service/employee-service.service.ts
@@ -1,36 +1,65 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { map } from 'rxjs';
-import * as fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
 
+interface Employee {
+  id: string;
+  avatar: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  birthday: string;
+  skills: string;
+  role: string;
+  is_admin: boolean;
+}
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeServiceService {
   private apiUrl = 'assets/data/employees.json';
-  private employees: any[] = []
-
+  private localStorageKey = 'employees';
+  employees: Employee[] = [];
   constructor(private http: HttpClient) {}
 
-  getEmployees(): Observable<any> {
-    return this.http.get<any>(this.apiUrl).pipe(
-      map(data => {
-        this.employees = data;
-        return this.employees
-      })
-    )
+  getEmployeesJson(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+  // getEmployees(): Observable<any[]> {
+  //   const employees = localStorage.getItem(this.storageKey)
+  //   if (employees != null)
+  //   {
+  //     const employees_json = JSON.parse(employees)
+  //     return employees_json
+
+  //   }
+  //   else
+  //   {
+
+  //   }
+  // }
+  getEmployees(): Observable<Employee[]> {
+    const employees = this.getEmployeesFromLocalStorage();
+    console.log(employees)
+    return of(employees);
   }
 
-  createEmployee(employee: any): Observable<any> { 
-    const data = fs.readFileSync(this.apiUrl, 'utf-8'); 
-    const employees = JSON.parse(data); 
-   
-    employees.push(employee); 
-     
-    const updatedData = JSON.stringify(employees, null, 2); 
-    // Write the updated JSON back to the file 
-    fs.writeFileSync(this.apiUrl, updatedData); 
-    return of(employee); 
+  createEmployee(employee: any): Observable<Employee> {
+    const employees = this.getEmployeesFromLocalStorage();
+    employee.id = uuidv4()
+    employee.skills.trim()
+    employees.push(employee);
+    this.saveEmployeesToLocalStorage(employees);
+    return of(employee);
+  }
+
+  public getEmployeesFromLocalStorage(): any[] {
+    const employeesJSON = localStorage.getItem(this.localStorageKey);
+    return employeesJSON ? JSON.parse(employeesJSON) : [];
+  }
+  public saveEmployeesToLocalStorage(employees: any[]): void {
+    const employeesJSON = JSON.stringify(employees);
+    localStorage.setItem(this.localStorageKey, employeesJSON);
   }
 }
