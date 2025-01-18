d758d508db89f7bc51a16bb72758f9417b1709e3
dang.le <dang06072000@gmail.com>
2025-01-19 01:29:11 +0700

    added the init template

---
+++ front-end/src/app/views/base/routes.ts
@@ -0,0 +1,34 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    data: {
+      title: 'Base'
+    },
+    children: [
+      {
+        path: '',
+        redirectTo: 'cards',
+        pathMatch: 'full'
+      },
+      {
+        path: 'accordion',
+        loadComponent: () => import('./accordion/accordions.component').then(m => m.AccordionsComponent),
+        data: {
+          title: 'Accordion'
+        }
+      },
+      {
+        path: 'breadcrumbs',
+        loadComponent: () => import('./breadcrumbs/breadcrumbs.component').then(m => m.BreadcrumbsComponent),
+        data: {
+          title: 'Breadcrumbs'
+        }
+      },
+   
+    ]
+  }
+];
+
+
