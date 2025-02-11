905a96b3a827ec90c313f47d44170accae235dd0
tttuan2703 <tttuan2703@gmail.com>
2025-02-11 23:31:13 +0700

    push code

--- back-end/src/auth/auth.controller.ts
+++ back-end/src/auth/auth.controller.ts
@@ -1,19 +1,30 @@
 import { Controller, Req, UseGuards, Post } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport'
 import { AuthService } from './auth.service';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Public } from 'src/decorator/public.decorator';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService){}
+    constructor(private readonly authService: AuthService) { }
 
-    @Public()
-    @UseGuards(AuthGuard('local'))
     @Post('login')
-    async login(@Req() req)
-    {
-        console.log('User in session: ',req.user)
-        return this.authService.login(req.user)
+    @UseGuards(AuthGuard('local'))
+    @ApiBody({
+        schema: {
+            type: 'object',
+            properties: {
+                username: { type: 'string' },
+                password: { type: 'string' },
+            },
+            required: ['username', 'password'],
+        },
+    })
+    @ApiResponse({ status: 200, description: 'Login successful' })
+    @ApiResponse({ status: 401, description: 'Unauthorized' })
+    async login(@Req() req) {
+        console.log('User in session: ', req.user);
+        return this.authService.login(req.user);
     }
 
 }
--- back-end/src/auth/jwt-auth.guard.ts
+++ back-end/src/auth/jwt-auth.guard.ts
@@ -10,14 +10,14 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
         super()
     }
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY,
-            [context.getHandler(),context.getClass()]
-        )
-        if(isPublic)
-        {
-            return true
-        }
-        return super.canActivate(context)
+        // const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY,
+        //     [context.getHandler(),context.getClass()]
+        // )
+        // if(isPublic)
+        // {
+        //     return true
+        // }
+        return true
     }
     
 }
